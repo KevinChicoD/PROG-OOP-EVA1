61de63e96e87f3494307381dbec37ac549de7c76
KevinChicoD <143564440+KevinChicoD@users.noreply.github.com>
2024-02-06 17:53:28 -0700

    Add files via upload

---
+++ EVA1-1-CLASES-OBJETOS/src/eva1/pkg1/clases/objetos/EVA1_1_CLASES_OBJETOS.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eva1.pkg1.clases.objetos;
+
+/**
+ *
+ * @author invitado
+ */
+public class EVA11CLASESOBJETOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Persona persol;//declaro un identificador (variable )de tipo persona 
+        //solo he declarado la variabke ,no he creado el objecto 
+        //crear el objecto (instanclar)consiste en asignar memoria.
+        int valor;
+    }
+    
+    CLASE PERSONA;
+    
+    NombreDeLaClase --> Nomeclatura CamelCase
+    Iss NombreDeLaClase { cuerpo de la clase}
+    
+    Iss Persona
+
+    private static class Persona {
+
+        public Persona() {
+        }
+    }
+            
+            
+}
